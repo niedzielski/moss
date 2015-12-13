a2e1739e8a4d16ea035784c3036f048e2a183ebb
Stephen Niedzielski <stephen@niedzielski.com>
2015-12-12 17:51:16 -0700

    Remove support for Aplite
    The Aplite SDK doesn't have support gpath_draw_outline_open(), similar
    to GL_LINE_STRIP, which I think will be very useful.

--- src/global.h
+++ src/global.h
@@ -3,8 +3,7 @@
 #include <pebble.h>
 
 #define log(...) APP_LOG(APP_LOG_LEVEL_DEBUG, __VA_ARGS__);
-#define argb2gcolor(argb) GColorFromRGBA(((argb) >> 16) & 0xff, \
-                                         ((argb) >>  8) & 0xff, \
-                                         ((argb) >>  0) & 0xff, \
-                                         ((argb) >> 24) & 0xff)
-#define gcolor(argb) COLOR_FALLBACK(argb2gcolor((argb)), (argb) ? (GColorBlack) : (GColorClear))
+#define gcolor(argb) GColorFromRGBA(((argb) >> 16) & 0xff, \
+                                    ((argb) >>  8) & 0xff, \
+                                    ((argb) >>  0) & 0xff, \
+                                    ((argb) >> 24) & 0xff)
