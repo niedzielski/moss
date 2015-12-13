3f40c9c0370c6942e08f0892b836dafde69ceb62
Stephen Niedzielski <stephen@niedzielski.com>
2015-12-13 01:08:35 -0700

    Initial drawing implementation
    Works fine in emulator but crashes PTR.

--- src/app.c
+++ src/app.c
@@ -1,39 +1,38 @@
 #include "global.h"
 #include "app.h"
+#include "gfx.h"
 
-static GPath * model_instance;
+#include "l_sys.h"
+
 static Layer * layer;
-
-static const GPathInfo MODEL = {
-  .num_points = 4,
-  .points = (GPoint []) {{0,0}, {0,-10}, {-10,-10}, {-10,0}}
-};
+static GRect win_bounds;
 
 void on_layer_update(Layer * layer, GContext * ctx) {
   graphics_context_set_stroke_color(ctx, gcolor(0xff00ff00));
-  gpath_draw_outline_open(ctx, model_instance);
-}
 
-void init_model_instance(GPoint origin) {
-  model_instance = gpath_create(&MODEL);
-  gpath_move_to(model_instance, origin);
+  l_sys_t sys = {
+    .axiom = "0",
+    .successors = (seq_t []) {"1[0]0", "11"},
+    .successors_len = 2
+  };
+
+  seq_t succession = succeed(&sys, 6);
+  log("succession=%s", succession);
+
+  draw(ctx, (GPoint) {win_bounds.size.w / 2, win_bounds.size.h}, 180, succession);
+
+  sdsfree(succession);
 }
 
 void on_window_load(Window * window) {
   Layer * window_layer = window_get_root_layer(window);
-  GRect bounds = layer_get_bounds(window_layer);
+  win_bounds = layer_get_bounds(window_layer);
 
-  layer = layer_create(bounds);
+  layer = layer_create(win_bounds);
   layer_set_update_proc(layer, on_layer_update);
   layer_add_child(window_layer, layer);
-
-  init_model_instance((GPoint) {
-    bounds.size.w / 2,
-    bounds.size.h
-  });
 }
 
 void on_window_unload(Window * window) {
   layer_destroy(layer);
-  gpath_destroy(model_instance);
 }
---
+++ src/gfx.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "global.h"
+#include "l_sys.h"
+
+void draw(GContext * ctx, GPoint origin, int angle, seq_t cmds);
