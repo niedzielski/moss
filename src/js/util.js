952a524d64160cf509df4465979bcb61d5b7934c
Stephen Niedzielski <stephen@niedzielski.com>
2015-12-20 21:31:17 -0700

    Fix log regex again
    In addition to the following two subroutine trace forms:
    
    • '     at app/index.js:46:7'
    • '     at Pebble.<anonymous> (main.js:5:8)'
    
    A third form was discovered:
    
    • '    at inbox.(anonymous function).recipient.origin.x (app/index.js:18:3)'
    
    The previous regex only expected the first two.

--- src/js/util.js
+++ src/js/util.js
@@ -4,7 +4,8 @@ var dbg = true;
 
 module.exports.log = dbg && function() {
   var sub = safe.translateError(new Error()).split('\n')[3];
-  var prefix = sub.replace(/\s*at\s*([^\s)(:]+)?\s+\(?([^:]+:)(\d+:)\d+\)?\s*/, '$2$3');
+  var prefixRegex = /\s*at\s*([^:]+)?\s+\(?([^:]+:)(\d+:)\d+\)?\s*/;
+  var prefix = sub.replace(prefixRegex, '$2$3');
   [].unshift.call(arguments, prefix);
   console.log.apply(null, arguments);
 } || function() {};
