ec80f49aef6223e66824330f34bdd743ff100b28
Stephen Niedzielski <stephen@niedzielski.com>
2015-12-25 22:35:39 -0700

    Replace global with function parameter

--- src/c/main.c
+++ src/c/main.c
@@ -1,38 +1,36 @@
 #include "global.h"
 #include "app.h"
 
-static Window * _win;
-
 static void on_window_load(Window * win) {
-  Layer * layer = window_get_root_layer(_win);
+  Layer * layer = window_get_root_layer(win);
   layer_set_update_proc(layer, on_layer_update);
 
   on_layer_init(layer);
 }
 
 static void on_window_unload(Window * win) {
-  on_layer_deinit(window_get_root_layer(_win));
+  on_layer_deinit(window_get_root_layer(win));
 }
 
-static void init() {
+static Window * init() {
   log("win init");
-  _win = window_create();
-  window_set_window_handlers(_win, (WindowHandlers) {
+  Window * win = window_create();
+  window_set_window_handlers(win, (WindowHandlers) {
     .load = on_window_load,
     .unload = on_window_unload,
   });
   const bool animated = true;
-  window_stack_push(_win, animated);
+  window_stack_push(win, animated);
+  return win;
 }
 
-static void deinit() {
-  window_destroy(_win);
-  _win = NULL;
+static void deinit(Window * win) {
+  window_destroy(win);
   log("win deinit");
 }
 
 int main() {
-  init();
+  Window * win = init();
   app_event_loop();
-  deinit();
+  deinit(win);
 }
